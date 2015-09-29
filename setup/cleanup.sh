4d8323e9864375e4697d156fef443e816ec1c730
subdir <subdir@gmail.com>
2015-09-29 21:45:49 +0300

    moved to dockerenv

--- setup/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-set -o pipefail
-
-if ! [[ -v DEVENV ]]; then
-    rm -f /etc/sudoers.d/user
-fi
-
--- setup/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-set -o pipefail
-
-apt-get update
-apt-get install --yes --no-install-recommends sudo runit
-echo "user ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/user
-
